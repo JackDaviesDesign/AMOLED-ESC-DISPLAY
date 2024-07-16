9ba8b9563d832da6b4b48985bc9900a74a32d08c
Jack Davies <jackdavies95@gmail.com>
2024-07-16 16:04:25 -0600

    Update esp32can_TESTING_2.ino
    Parsed all periodically sent messages over the CAN-BUS.

--- esp32can_TESTING_2/esp32can_TESTING_2.ino
+++ esp32can_TESTING_2/esp32can_TESTING_2.ino
@@ -5,8 +5,51 @@
 CAN_device_t CAN_cfg; // CAN Config
 const int rx_queue_size = 10;
 
-const uint32_t filterId1 = 0x618; // CAN ID to filter
-const uint32_t filterId2 = 0x620; // CAN ID to filter
+// Change CAN_SPEED & Tx,Rx pins in setup below to match hardware. Silixcon default CAN speed is 1000KBPS
+
+// lynxStatus
+uint8_t currentPowerMap;
+uint8_t driverStatusWord;
+uint16_t driverLimitWord;
+uint16_t driverErrorWord;
+
+// lynxMotorStatus
+uint16_t motorCurrent; // Motor current [A] (q axis)
+uint16_t motorRpm; // Motor speed [rpm]
+uint16_t vehicleSpeed; // Vehicle speeed [km/h]
+uint16_t motorWatts; // Motor power [W]
+
+// lynxBatteryStatus
+uint8_t mapType;  // Map type: 1 - Normal   2 - Restricted   3 - reverse   4 - boost   5 - Reserve map
+uint8_t SOC; // SOC [%]
+uint16_t battVoltage; // Battery voltage [0.01 V]
+uint16_t battCurrent; // Battery current [0.02 A]
+
+// ODO_trip
+float TRIP; // TRIP [0.01 km ], distance counter. Can by reset from display or CAN command.
+float ODO; // ODO [0.1 km], total distance counter
+
+// relativeValues   -   Mostly used by the display for bargrafs
+uint16_t relMotorPhaseCurrent; // Relative motor phase current -32767 - 32767. (32767 = iref)
+uint16_t relDriverTotLimit; // Driver total limit. 32767 = Full power  0 = Zero power (100% limitation)
+uint16_t relSpeed; // Relative speed (-323767 - 32767). Only works if parameter /maps/maxkph is set. The parameter is the full value.
+
+// temps
+uint16_t motorRThermistor; // Driver /driver/motor/RThermistor  Disconnected sensor = 0xFFFF
+uint16_t ptcTemp; // Driver /driver/ptctemp
+uint8_t drivetTemp; // Driver /drivet/temp
+
+
+
+
+// CAN IDs to filter - https://docs.silixcon.com/docs/fw/apps/esc/lynx/can/periodic_messages
+const uint32_t lynxStatus = 0x600; // Sent every 100ms
+const uint32_t lynxMotorStatus = 0x610; // Sent every 100ms
+const uint32_t lynxBatteryStatus = 0x618; // Sent every 100ms
+const uint32_t ODO_trip = 0x620; // Sent every 1s
+const uint32_t rangeEstimator = 0x625; // Sent every 800ms - Range estimator feature is not finished yet. It is not recommended to use it.
+const uint32_t relativeValues = 0x626; // Sent every 250ms
+const uint32_t temps = 0x628; // Sent every 800ms
 
 
 void setup() {
@@ -32,10 +75,25 @@ void setup() {
 
 void loop() {
 
+recieveCANData();
+
+}
+
+
+
+
+
+
+void recieveCANData(){
+
   // Check for received CAN messages
   CAN_frame_t rx_frame;
   if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
-    if (rx_frame.MsgID == filterId1) { // Check if the message ID matches the filter ID
+    
+    
+    // ---------- lynxStatus - 0x600 ----------
+
+    if (rx_frame.MsgID == lynxStatus) { // Check if the message ID matches the filter ID
       Serial.print("Filtered CAN message received, ID: 0x");
       Serial.println(rx_frame.MsgID, HEX);
 
@@ -48,20 +106,20 @@ void loop() {
 
       if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
         // Extracting values considering little-endian format
-        uint8_t SOC = data[2]; // 8-bit integer // SOC %
-        uint8_t SOH = data[3]; // 8-bit integer // SOH %
-        float battV = data[4] | (data[5] << 8); // 16-bit integer // batt voltage
-        float battA = data[6] | (data[7] << 8); // 16-bit integer // batt current        
+        currentPowerMap = data[2]; // 8-bit integer
+        driverStatusWord = data[3]; // 8-bit integer
+        driverLimitWord = data[4] | (data[5] << 8); // 16-bit integer
+        driverErrorWord = data[6] | (data[7] << 8); // 16-bit integer       
 
         // Print the extracted values
-        Serial.print("SOC: ");
-        Serial.println(SOC);
-        Serial.print("SOH: ");
-        Serial.println(SOH);
-        Serial.print("Batt V: ");
-        Serial.println(battV/100);
-        Serial.print("Batt A: ");
-        Serial.println(battA/100);
+        Serial.print("Current Power Map: ");
+        Serial.println(currentPowerMap);
+        Serial.print("Driver Status Word: ");
+        Serial.println(driverStatusWord);
+        Serial.print("Driver Limit Word: ");
+        Serial.println(driverLimitWord);
+        Serial.print("Driver Error Word: ");
+        Serial.println(driverErrorWord);
 
       } else {
         Serial.println("Received data length is less than expected.");
@@ -72,7 +130,89 @@ void loop() {
 
 
 
-if (rx_frame.MsgID == filterId2) { // Check if the message ID matches the filter ID
+// ---------- lynxMotorStatus - 0x610 ----------
+
+    if (rx_frame.MsgID == lynxMotorStatus) { // Check if the message ID matches the filter ID
+      Serial.print("Filtered CAN message received, ID: 0x");
+      Serial.println(rx_frame.MsgID, HEX);
+
+      // Read the incoming data
+      uint8_t data[8];
+      int dataLength = rx_frame.FIR.B.DLC; // Get the number of bytes in the received message
+      for (int i = 0; i < dataLength; i++) {
+        data[i] = rx_frame.data.u8[i];
+      }
+
+      if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
+        // Extracting values considering little-endian format
+        motorCurrent = data[0] | (data[1] << 8); // 16-bit integer
+        motorRpm = data[2] | (data[3] << 8); // 16-bit integer
+        vehicleSpeed = data[4] | (data[5] << 8); // 16-bit integer
+        motorWatts = data[6] | (data[7] << 8); // 16-bit integer     
+
+        // Print the extracted values
+        Serial.print("Motor Current: ");
+        Serial.println(motorCurrent);
+        Serial.print("Motor RPM: ");
+        Serial.println(motorRpm);
+        Serial.print("Vehicle Speed: ");
+        Serial.println(vehicleSpeed);
+        Serial.print("Motor Watts: ");
+        Serial.println(motorWatts);
+
+      } else {
+        Serial.println("Received data length is less than expected.");
+      }
+    }
+
+
+
+
+
+
+
+    // ---------- lynxBatteryStatus - 0x618 ----------
+
+    if (rx_frame.MsgID == lynxBatteryStatus) { // Check if the message ID matches the filter ID
+      Serial.print("Filtered CAN message received, ID: 0x");
+      Serial.println(rx_frame.MsgID, HEX);
+
+      // Read the incoming data
+      uint8_t data[8];
+      int dataLength = rx_frame.FIR.B.DLC; // Get the number of bytes in the received message
+      for (int i = 0; i < dataLength; i++) {
+        data[i] = rx_frame.data.u8[i];
+      }
+
+      if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
+        // Extracting values considering little-endian format
+        mapType = data[1]; // 8-bit integer // SOC %
+        SOC = data[2]; // 8-bit integer // SOH %
+        battVoltage = data[4] | (data[5] << 8); // 16-bit integer // batt voltage
+        battCurrent = data[6] | (data[7] << 8); // 16-bit integer // batt current        
+
+        // Print the extracted values
+        Serial.print("Map Type : ");
+        Serial.println(mapType);
+        Serial.print("SOC: ");
+        Serial.println(SOC);
+        Serial.print("Batt Voltage: ");
+        Serial.println(battVoltage/100);
+        Serial.print("Batt Current: ");
+        Serial.println(battCurrent/100);
+
+      } else {
+        Serial.println("Received data length is less than expected.");
+      }
+    }
+
+
+
+
+
+    // ---------- ODO_trip - 0x620 ----------
+
+if (rx_frame.MsgID == ODO_trip) { // Check if the message ID matches the filter ID
       Serial.print("Filtered CAN message received, ID: 0x");
       Serial.println(rx_frame.MsgID, HEX);
 
@@ -86,32 +226,72 @@ if (rx_frame.MsgID == filterId2) { // Check if the message ID matches the filter
       if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
         // Extracting values considering little-endian format      
 
-        // // 32-bit float - TRIP
-        // union {
-        //   uint8_t b[4];
-        //   float f;
-        // } TRIP;
-        // TRIP.b[0] = data[0];
-        // TRIP.b[1] = data[1];
-        // TRIP.b[2] = data[2];
-        // TRIP.b[3] = data[3];
+        // 32-bit float - TRIP
+        union {
+          uint8_t b[4];
+          float f;
+        } TRIPf;
+        TRIPf.b[0] = data[0];
+        TRIPf.b[1] = data[1];
+        TRIPf.b[2] = data[2];
+        TRIPf.b[3] = data[3];
+
+        TRIP = TRIPf.f; // assign the 4 bytes to a float
+
 
         // 32-bit float - ODO
         union {
           uint8_t b[4];
           float f;
-        } ODO;
-        ODO.b[0] = data[4];
-        ODO.b[1] = data[5];
-        ODO.b[2] = data[6];
-        ODO.b[3] = data[7];
+        } ODOf;
+        ODOf.b[0] = data[4];
+        ODOf.b[1] = data[5];
+        ODOf.b[2] = data[6];
+        ODOf.b[3] = data[7];
 
-        // Serial.print("TRIP : ");
-        // Serial.println(TRIP.f);
+        ODO = ODOf.f; // assign the 4 bytes to a float
+
+
+        Serial.print("TRIP : ");
+        Serial.println(TRIP);
 
         Serial.print("ODO : ");
-        Serial.println(ODO.f);
+        Serial.println(ODO);
 
+
+      } else {
+        Serial.println("Received data length is less than expected.");
+      }
+    }
+
+
+// ---------- relativeValues - 0x626 ----------
+
+    if (rx_frame.MsgID == relativeValues) { // Check if the message ID matches the filter ID
+      Serial.print("Filtered CAN message received, ID: 0x");
+      Serial.println(rx_frame.MsgID, HEX);
+
+      // Read the incoming data
+      uint8_t data[8];
+      int dataLength = rx_frame.FIR.B.DLC; // Get the number of bytes in the received message
+      for (int i = 0; i < dataLength; i++) {
+        data[i] = rx_frame.data.u8[i];
+      }
+
+      if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
+        // Extracting values considering little-endian format
+        relMotorPhaseCurrent = data[0] | (data[1] << 8); // 16-bit integer // batt voltage
+        relDriverTotLimit = data[2] | (data[3] << 8); // 16-bit integer // batt current  
+        relSpeed = data[4] | (data[5] << 8); // 16-bit integer // batt voltage
+      
+
+        // Print the extracted values
+        Serial.print("Relative Motor Phase Current : ");
+        Serial.println(relMotorPhaseCurrent);
+        Serial.print("Relative Driver Total Limit: ");
+        Serial.println(relDriverTotLimit);
+        Serial.print("Relative Speed: ");
+        Serial.println(relSpeed);
 
       } else {
         Serial.println("Received data length is less than expected.");
@@ -121,11 +301,57 @@ if (rx_frame.MsgID == filterId2) { // Check if the message ID matches the filter
 
 
 
+    // ---------- temps - 0x628 ----------
 
+    if (rx_frame.MsgID == temps) { // Check if the message ID matches the filter ID
+      Serial.print("Filtered CAN message received, ID: 0x");
+      Serial.println(rx_frame.MsgID, HEX);
+
+      // Read the incoming data
+      uint8_t data[8];
+      int dataLength = rx_frame.FIR.B.DLC; // Get the number of bytes in the received message
+      for (int i = 0; i < dataLength; i++) {
+        data[i] = rx_frame.data.u8[i];
+      }
+
+      if (dataLength >= 7) { // Ensure there are at least 7 bytes of data
+        // Extracting values considering little-endian format
+
+        motorRThermistor = data[0] | (data[1] << 8); // 16-bit integer // batt voltage
+        ptcTemp = data[2] | (data[3] << 8); // 16-bit integer // batt current        
+        drivetTemp = data[4]; // 8-bit integer // SOC %
+
+
+        // Print the extracted values
+        Serial.print("R Thermistor : ");
+        Serial.println(motorRThermistor);
+        Serial.print("PTC Temp: ");
+        Serial.println(ptcTemp);
+        Serial.print("Drive T Temp: ");
+        Serial.println(drivetTemp);
+
+      } else {
+        Serial.println("Received data length is less than expected.");
+      }
+    }
 
 
   }
 }
+
+void draw() {  // print to display
+
+  // display code here
+
+}
+
+
+
+
+
+
+
+
   // // Check for received CAN messages
   // CAN_frame_t rx_frame;
   // if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
